bdf8c58ba7d9986f98ed9187c1162ee0047134f2
DaveVanVerdegem <dave.dendave@gmail.com>
2015-01-24 02:34:08 +0100

    Health script added Bomb prefab + script added

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour 
+{
+	public float health = 3;
+	public float maxHealth = 3;
+	public float regenRate = 0;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if(health <= 0)
+		{
+			Destroy (this.gameObject);
+		}
+
+		//Regenerate health
+		health += Time.deltaTime * regenRate;
+		health = Mathf.Clamp(health, 0, maxHealth);
+	}
+}
